328dd1e15d9ba29c22a726a4ca5e689b4e64e0b2
vgbhj <mishaslsk@gmail.com>
2024-11-03 23:24:56 +0300

    Done Return and handle an error

--- Tutorial: Create a module/hello/hello.go
+++ Tutorial: Create a module/hello/hello.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Miha")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
