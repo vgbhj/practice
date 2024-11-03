331327563f7577fb28ffc5421d90216fc247cab4
vgbhj <mishaslsk@gmail.com>
2024-11-04 00:42:34 +0300

    Return greetings for multiple people

--- Tutorial: Create a module/hello/hello.go
+++ Tutorial: Create a module/hello/hello.go
@@ -14,12 +14,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	// Request a greeting message
-	message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
