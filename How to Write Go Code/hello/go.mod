55314d59297cf5d88a1539f2bcf9d3003db5bb56
vgbhj <mishaslsk@gmail.com>
2024-12-11 23:24:27 +0300

    Routing (using gorilla/mux) start

--- How to Write Go Code/hello/go.mod
+++ How to Write Go Code/hello/go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.23.4
+
+require github.com/google/go-cmp v0.6.0
--- How to Write Go Code/hello/hello.go
+++ How to Write Go Code/hello/hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
