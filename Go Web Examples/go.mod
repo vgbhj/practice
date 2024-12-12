e5613354114ab3a866c4c498211f53d116d6b636
vgbhj <mishaslsk@gmail.com>
2024-12-12 19:48:33 +0300

    Passed routing

--- Go Web Examples/go.mod
+++ Go Web Examples/go.mod
@@ -2,4 +2,8 @@ module example.com/go_web_example
 
 go 1.23.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- Go Web Examples/main.go
+++ Go Web Examples/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":5000", r)
 }
